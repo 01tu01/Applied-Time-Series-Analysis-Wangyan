7c9da1431a6c6e511df5ddbc458bbcbbd423f001
01tu01 <87894726+01tu01@users.noreply.github.com>
2022-09-16 15:40:48 +0800

    上传出

---
+++ ch3-ex.R
@@ -0,0 +1,28 @@
+# ch3-ex
+setwd("D:\\undergraduate\\研\\432统计学\\应用时间序列分析资料\\R解答")
+library(openxlsx)
+library(tseries)
+
+## ex16
+rm(list=ls(all=TRUE))
+E3_16 <- read.xlsx(".\\datafile\\E2_7.xlsx", sheet=1)
+x <- ts(E3_16$mortality, start=c(1915,1))
+plot(x)
+acf(x, lag.max=24)
+adf.test(x)
+pacf(x)
+plot(diff(x))
+acf(diff(x), lag.max=24)
+pacf(diff(x))
+
+## ex17
+rm(list=ls(all=TRUE))
+E3_17 <- read.xlsx(".\\datafile\\E2_8.xlsx", sheet=1)
+x <- ts(E3_17$wl, start=c(1860,1))
+plot(x)
+acf(x, lag.max=24)
+adf.test(x)
+pacf(x)
+plot(diff(x))
+acf(diff(x), lag.max=24)
+pacf(diff(x))
